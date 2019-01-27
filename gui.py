b2a026e6ab0a24247334ff285ef434752f7e5d43
DTT_Darkman <dtt_darkman@hotmail.com>
2019-01-27 17:37:01 -0600

    More bits of GUI.
    Signed-off-by: DTT_Darkman <dtt_darkman@hotmail.com>

--- gui.py
+++ gui.py
@@ -10,7 +10,7 @@ results = relevance_sort("summer", matches)
 # Main window config
 root = Tk()
 root.config(bg="gray30")
-root.geometry("900x600")
+root.geometry("1000x600")
 root.title("Sloth")
 root.iconbitmap("wco_icon.ico")
 
@@ -22,8 +22,7 @@ s.map('TButton', foreground=[('pressed', 'OrangeRed4')])
 s.configure('TFrame', background="gray30")
 
 
-
-# Serach bar
+# Search bar
 search_bar = Frame(root)
 search_bar.grid(row=0, columnspan=4, sticky=W)
 criteria = Entry(search_bar, width=50)
@@ -39,6 +38,7 @@ dub_ser = Radiobutton(ser_type, text="Dubbed Anime", value=2)
 sub_ser = Radiobutton(ser_type, text="Subbed Anime", value=3)
 mov_ser = Radiobutton(ser_type, text="Movies", value=4)
 car_ser = Radiobutton(ser_type, text="Cartoons", value=5)
+
 all_ser.grid(row=0, sticky=W, column=0)
 dub_ser.grid(row=0, sticky=W, column=1)
 sub_ser.grid(row=0, sticky=W, column=2)
@@ -46,8 +46,10 @@ mov_ser.grid(row=0, sticky=W, column=3)
 car_ser.grid(row=0, sticky=W, column=4)
 
 
-# Results listbox
-resultsbox = Listbox(root, width=120, height=15, bd=3,
+# Listboxes
+boxes = Frame(root)
+boxes.grid(row=2, sticky=W)
+resultsbox = Listbox(boxes, width=75, height=15, bd=3,
                      bg="gray20", fg="SlateGray3",
                      activestyle="none",
                      selectforeground="OrangeRed4",
@@ -55,10 +57,19 @@ resultsbox = Listbox(root, width=120, height=15, bd=3,
                      selectmode=SINGLE)
 for item in results:
     resultsbox.insert(END, item)
-resultsbox.grid(row=5, columnspan=4, sticky=E+W)
+resultsbox.grid(row=1, columnspan=4, sticky=W)
 
-Button(root, text="Add").grid(row=5, column=4, padx=3)
+queue = Listbox(boxes, width=75, height=15, bd=3,
+                bg="gray20", fg="SlateGray3",
+                activestyle="none",
+                selectforeground="OrangeRed4",
+                selectbackground="DodgerBlue3",
+                selectmode=SINGLE)
+queue.grid(row=1, column=7, columnspan=4, sticky=W)
+
+controls = Frame(boxes)
+controls.grid(row=1, column=6, sticky=W)
+Button(controls, text="Remove").grid(row=2, padx=3)
+Button(controls, text="Add").grid(row=1, padx=3)
 
 root.mainloop()
-
-
